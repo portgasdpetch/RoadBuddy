7f6b8d530447698ee41261e77c2aa75f185987f2
Testv01 <frozen_lucifer@hotmail.com>
2018-10-22 00:05:12 +0700

    [Add] Delete button
    +refresh on delete
    + that thing
    -still buggy at geolocation

--- js/app.js
+++ js/app.js
@@ -4,3 +4,37 @@ window.onload=function(){
         console.log(dataSnapshot.val());
     });
 }
+
+function delOnClick(param1){
+    var firebaseRef=firebase.database().ref("places/"+param1);
+    firebaseRef.remove().then(function(){
+        location.reload();
+    }).catch(function(error){
+        console.log("NOPE");
+    })
+}
+
+function getAddress (latitude, longitude) {
+    return new Promise(function (resolve, reject) {
+        var request = new XMLHttpRequest();
+
+        var method = 'GET';
+        var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true';
+        var async = true;
+
+        request.open(method, url, async);
+        request.onreadystatechange = function () {
+            if (request.readyState == 4) {
+                if (request.status == 200) {
+                    var data = JSON.parse(request.responseText);
+                    var address = data.results[0];
+                    resolve(address.formatted_address);
+                }
+                else {
+                    reject(request.status);
+                }
+            }
+        };
+        request.send();
+    });
+};
