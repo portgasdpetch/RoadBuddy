71a38c72a8c52b059edc6f1d7dff1b2b78a6d091
Testv01 <frozen_lucifer@hotmail.com>
2018-10-23 18:23:23 +0700

    [Add] Location name

--- js/app.js
+++ js/app.js
@@ -14,12 +14,12 @@ function delOnClick(param1){
     })
 }
 
-function getAddress (latitude, longitude) {
+function getAddress (latitude, longitude,int) {
     return new Promise(function (resolve, reject) {
         var request = new XMLHttpRequest();
 
         var method = 'GET';
-        var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true';
+        var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true&key=AIzaSyB_1gqNDYMyc10X_3lp5qh2iTM3DlAm4gE';
         var async = true;
 
         request.open(method, url, async);
@@ -29,6 +29,7 @@ function getAddress (latitude, longitude) {
                     var data = JSON.parse(request.responseText);
                     var address = data.results[0];
                     resolve(address.formatted_address);
+                    document.getElementById("someId"+int).innerHTML = address.formatted_address;                   
                 }
                 else {
                     reject(request.status);
