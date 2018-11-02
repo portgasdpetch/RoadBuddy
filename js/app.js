06e4b16cd2a8aafa5ebb56c28a936ad9a04f7e54
petch <petchportgas@gmail.com>
2018-11-02 15:23:34 +0700

    2.0

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 window.onload = function () {
     var firebaseRef = firebase.database().ref("places");
     firebaseRef.once('value').then(function (dataSnapshot) {
@@ -16,74 +15,93 @@ function delOnClick(param1) {
     })
 }
 
-addOnClick=(param2)=>{
-       const data =  database.ref("Report").child(param2).once('value',(snapshot)=>{
-           const data = snapshot.val()
-            firebase.database().ref("Process").push({
-            latitude:data.latitude,
-            longitude:data.longitude,
-            topic:data.topic,
-            description:data.description,
-
+addOnClick = (param2) => {
+    const data = database.ref("Report").child(param2).once('value', (snapshot) => {
+        const data = snapshot.val()
+        firebase.database().ref("Process").push({
+            latitude: data.latitude,
+            longitude: data.longitude,
+            topic: data.topic,
+            description: data.description,
+            timestamp:Date.now()
         })
-    }).then(value=>{
-        if(value){
+    }).then(value => {
+        if (value) {
             database.ref("Report").child(param2).remove()
             location.reload();
         }
     })
+
+
     // database.ref("Report").child(param2).remove()
 }
+DoneOnClick = (param3) => {
+    const data = database.ref("Process").child(param3).once('value', (snapshot) => {
+        const data = snapshot.val()
+        firebase.database().ref("Finish").push({
+            latitude: data.latitude,
+            longitude: data.longitude,
+            topic: data.topic,
+            description: data.description,
 
-// function addOnClick(param2){
-//    const test = firebase.database().ref("Report").child(param2)
-//     console.log(test)
-//     // firebaseRef.push({
-//     //     latitude: a,
-//     //     longitude: b,
-//     //     topic: c,
-//     //     description: d,
-//     //     image: e,        
-//     // })
-// }
-
-function getAddress(latitude, longitude, id) {
-    return new Promise(function (resolve, reject) {
-        var request = new XMLHttpRequest();
-
-        var method = 'GET';
-        var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true&key=AIzaSyB_1gqNDYMyc10X_3lp5qh2iTM3DlAm4gE';
-        var async = true;
-
-        request.open(method, url, async);
-        request.onreadystatechange = function () {
-            if (request.readyState == 4) {
-                if (request.status == 200) {
-                    var data = JSON.parse(request.responseText);
-                    var address = data.results[0];
-                    resolve(address.formatted_address);
-                    document.getElementById(id).innerHTML = address.formatted_address;
-                    console.log(address.formatted_address)
-                }
-                else {
-                    reject(request.status);
-                }
-            }
-        };
-        request.send();
-    });
-};
-
-function insertProcess(a, b, c, d, e) {
-    firebase.database().ref("Process").push({
-        latitude: a,
-        longitude: b,
-        topic: c,
-        description: d,
-        image: e,
-        date: firebase.database.ServerValue.TIMESTAMP
+        })
+    }).then(value => {
+        if (value) {
+            database.ref("Process").child(param3).remove()
+            location.reload();
+        }
     })
-};
+}
+
+    // function addOnClick(param2){
+    //    const test = firebase.database().ref("Report").child(param2)
+    //     console.log(test)
+    //     // firebaseRef.push({
+    //     //     latitude: a,
+    //     //     longitude: b,
+    //     //     topic: c,
+    //     //     description: d,
+    //     //     image: e,        
+    //     // })
+    // }
+
+    function getAddress(latitude, longitude, id) {
+        return new Promise(function (resolve, reject) {
+            var request = new XMLHttpRequest();
+
+            var method = 'GET';
+            var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true&key=AIzaSyB_1gqNDYMyc10X_3lp5qh2iTM3DlAm4gE';
+            var async = true;
+
+            request.open(method, url, async);
+            request.onreadystatechange = function () {
+                if (request.readyState == 4) {
+                    if (request.status == 200) {
+                        var data = JSON.parse(request.responseText);
+                        var address = data.results[0];
+                        resolve(address.formatted_address);
+                        document.getElementById(id).innerHTML = address.formatted_address;
+                        console.log(address.formatted_address)
+                    }
+                    else {
+                        reject(request.status);
+                    }
+                }
+            };
+            request.send();
+        });
+    };
+
+    function insertProcess(a, b, c, d, e) {
+        firebase.database().ref("Process").push({
+            latitude: a,
+            longitude: b,
+            topic: c,
+            description: d,
+            image: e,
+            date: firebase.database.ServerValue.TIMESTAMP
+        })
+    };
 
 // export default{
 //     insertProcess
